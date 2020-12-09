4cbc14b343c66a81581dbc23f9ba636cde3ea49d
imkrish513 <70813672+imkrish513@users.noreply.github.com>
2020-12-09 11:55:15 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var database;
+var form,game,player;
+var gameState = 0;
+var playerCount = 0;
+var backgroundIMG;
+var allPlayers
+var car1,car2,car3,car4
+function setup(){
+    database = firebase.database()
+    createCanvas(displayWidth-200,displayHeight-30);
+game = new Game()
+game.getState()
+game.start()
+}
+
+function draw(){
+   if(playerCount === 4){
+       game.update(1)
+   }
+   if(gameState === 1 ){
+       clear()
+       game.play()
+   }
+
+}
